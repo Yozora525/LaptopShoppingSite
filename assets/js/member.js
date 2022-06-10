8f438da5e57d8f67fbf017a133fc0152fb801729
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2022-06-10 15:35:48 +0800

    20220610-shiyan

--- assets/js/member.js
+++ assets/js/member.js
@@ -16,9 +16,11 @@ $(function(){
         if(pwd != check_pwd){
             $('#check-pwd-tip').html('兩次密碼輸入不一致');
             $('#change-pwd').prop('disabled', true);
-        }else{
+        }else if(reg.test(pwd)){
             $('#check-pwd-tip').html('');
             $('#change-pwd').prop('disabled', false);
+        }else{
+            $('#change-pwd').prop('disabled', true);
         }
         
     });
@@ -26,15 +28,19 @@ $(function(){
     $('#check-pwd').on('keyup', function(event){
         let pwd = $('#pwd').val();
         let check_pwd = $(this).val();
+        let reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$/;
 
         if(pwd != check_pwd){
             $('#check-pwd-tip').html('兩次密碼輸入不一致');
             $('#change-pwd').prop('disabled', true);
-        }else{
+        }else if(reg.test(check_pwd)){
             $('#check-pwd-tip').html('');
             $('#change-pwd').prop('disabled', false);
+        }else{
+            $('#change-pwd').prop('disabled', true);
         }
     });
+
 
 
 });
